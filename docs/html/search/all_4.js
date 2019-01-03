de200c07fd36ed57544ae7ea3f7d6e4b318638a3
Andrea Lorenzon <andrelorenzon@gmail.com>
2019-01-03 14:21:42 +0100

    deleted duplicate folders

--- docs/html/search/all_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['find',['find',['../class_tree.html#a1136c005709386b07d8a871765d59836',1,'Tree']]]
-];
--- docs/html/search/functions_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['_7etree',['~Tree',['../class_tree.html#a96e00b300d10c071dab6a814d986647d',1,'Tree']]]
-];
